c5b03bd777b735b7f8d1ff6d6f1f8e86b776a183
adewale <adewalekujore6@gmail.com>
2023-10-28 12:58:03 +0100

    update

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ url="https://github.com/alx-tools/Betty.git"
 
 git clone "$url"
 
-cd "Betty"
+
 
 file="install.sh"
 
 if [ -f "$file" ]; then
-        sudo "./$file"
-        cat ../package > betty
-        chmod a+x betty
-        sudo mv betty /bin/
-        cd ..
+        sudo "./Betty/$file"
+        cat package > Betty/betty
+        chmod a+x Betty/betty
+        sudo mv Betty/betty /bin/
+     
 
         betty "$@"
 
