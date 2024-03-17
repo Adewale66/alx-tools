ea050206a0aa7c5586bc2e89329ccc278b20ee55
Wale Kujore <adewalekujore6@gmail.com>
2024-03-17 20:52:00 +0100

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Check if the file exists
-
-if [ -e .github/workflows/m*.yaml ]; then
-	if [ -s .github/workflows/m*.yaml ]; then
-		:
-	else
-		echo "The YAML file is empty"
-		exit 1
-	fi
-else
-  echo "The YAML file does not exist"
-  exit 1
-fi
-
 # Check if the README exists and is not empty
 
 if [ -e README.md ]; then
